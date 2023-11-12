0711f0a1188c97a06971b2019804f0da0e41e6de
Jesus Ariel Gonzalez Bonilla <ariel5253@hotmail.com>
2023-11-12 09:38:10 -0500

    Cambios

--- 17-Sesion-12-11/Script SQL.sql
+++ 17-Sesion-12-11/Script SQL.sql
@@ -31,10 +31,16 @@ CREATE TABLE materia(
 CREATE TABLE matricula(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	codigo VARCHAR(6) NOT NULL ,
-	nota DECIMAL(10,2),
+	nota DECIMAL(10,2) NULL,
 	observacion VARCHAR(200) NOT NULL ,
-	estado BIT DEFAULT 1	
+    id_estudiante INT NOT NULL,
+    id_materia INT NOT NULL,
+	estado BIT DEFAULT 1,
+    FOREIGN KEY (id_estudiante) REFERENCES estudiante(id),
+    FOREIGN KEY (id_materia) REFERENCES materia(id)
 );
+
+ 
 
 -- DML 
 
@@ -47,3 +53,25 @@ INSERT INTO estudiante(tipo_documento, documento, nombre, apellido, correo,	dire
 ('CC','1068','Diana','Bonilla','diana@','Calle 19',1),
 ('CC','1020','Jorge','Suaza','jorge@','Calle 20',1);
 
+-- Insertar materias
+INSERT INTO materia(codigo, nombre,	descripcion, numero_trimestre, estado) VALUES
+('101012', 'Algoritmos', 'Documentación y realización de algoritmos', 1, 1),
+('101013', 'Requerimientos', 'Documentación del proyecto', 1, 1),
+('101013', 'Programación POO', 'Costruir clases', 2, 1),
+('101014', 'Diseño WEB', 'Costruir apliaciones WEB', 3, 1);
+
+INSERT INTO  matricula (codigo, nota, observacion, estado, id_materia, id_estudiante) VALUES
+('56', 90.5, 'Buena participación', 1, 1, 1),
+('89', 85.0, 'Ni bueno ni malo', 1, 2, 2),
+('89', 78.3, 'Métale más ganas', 0, 3, 3);
+
+INSERT INTO  matricula (codigo, observacion, estado, id_materia, id_estudiante) VALUES
+('ABC123', 'Aprobado', 1,2,1),
+('XYZ789', 'Regular', 1,3,1),
+('DEF456', 'Bueno', 1,4,4),
+('GHI098', 'Suficiente', 1,4,4);
+
+-- Consultas
+SELECT * FROM estudiante;
+SELECT * FROM materia;
+SELECT * FROM matricula;
